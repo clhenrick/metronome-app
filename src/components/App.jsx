b9334c31a25f058b315732fa3efb7e5ec897003f
Chris Henrick <chrishenrick@gmail.com>
2017-12-31 20:01:44 -0800

    check togglePlayPause() action creator works

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { init } from '../utils/metronome';
 
 class App extends Component {
+  static propTypes = {
+    togglePlayPause: PropTypes.func.isRequired,
+  };
+
   componentDidMount() {
     init();
+
+    // manually invoke to make sure it's working
+    this.props.togglePlayPause();
+    setTimeout(() => {
+      this.props.togglePlayPause();
+    }, 5000);
   }
 
   render() {
