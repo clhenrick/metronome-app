b19fc7a1cd4aaa7348984d4bea6f7b8d0153c9a9
Chris Henrick <chrishenrick@gmail.com>
2017-12-31 20:00:09 -0800

    added custom redux middleware for metronome

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,95 @@
+// Redux Middleware configuration
+// see: https://redux.js.org/docs/advanced/Middleware.html
 import logger from 'redux-logger';
+import {
+  play,
+  setTempo,
+  setMeter,
+  setMasterVolume,
+  setAccentVolume,
+  setQuarterVolume,
+  setEigthVolume,
+  setSixteenthVolume,
+  setTripletVolume,
+} from './utils/metronome';
+import {
+  TOGGLE_PLAY_PAUSE,
+  SET_TEMPO,
+  SET_METER,
+  SET_MASTER_VOLUME,
+  SET_ACCENT_VOLUME,
+  SET_QUARTER_VOLUME,
+  SET_EIGTH_VOLUME,
+  SET_SIXTEENTH_VOLUME,
+  SET_TRIPLET_VOLUME,
+} from './constants';
 
+// all Redux middleware will be exported within this array, aka the "middleware chain"
 const middleware = [];
+
+// this middleware passes redux state updates to relevant metronome logic
+// there's probably a more elegant or less redundant way of doing this!
+const metronomeMiddleware = store => next => action => {
+  // intercept whatever action is being passed to redux, and then get parts of state to pass to the metronome
+  // here state reflects changes already made by the reducer as the action is dispatched via next(action)
+  const result = next(action);
+  const {
+    isPlaying,
+    tempo,
+    meter,
+    masterVolume,
+    accentVolume,
+    quarterVolume,
+    eighthVolume,
+    sixteenthVolume,
+    tripletVolume,
+  } = store.getState();
+
+  switch (action.type) {
+    case TOGGLE_PLAY_PAUSE:
+      play(isPlaying);
+      break;
+
+    case SET_TEMPO:
+      setTempo(tempo);
+      break;
+
+    case SET_METER:
+      setMeter(meter);
+      break;
+
+    case SET_MASTER_VOLUME:
+      setMasterVolume(masterVolume);
+      break;
+
+    case SET_ACCENT_VOLUME:
+      setAccentVolume(accentVolume);
+      break;
+
+    case SET_QUARTER_VOLUME:
+      setQuarterVolume(quarterVolume);
+      break;
+
+    case SET_EIGTH_VOLUME:
+      setEigthVolume(eighthVolume);
+      break;
+
+    case SET_SIXTEENTH_VOLUME:
+      setSixteenthVolume(sixteenthVolume);
+      break;
+
+    case SET_TRIPLET_VOLUME:
+      setTripletVolume(tripletVolume);
+      break;
+
+    default:
+  }
+
+  return result;
+};
+
+// add the metronome middleware to the middleware chain
+middleware.push(metronomeMiddleware);
 
 if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
   // redux-logger only works in a browser environment
