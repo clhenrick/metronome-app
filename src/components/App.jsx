c29be8f52be76249c2d88ac122110d69494b65ea
Chris Henrick <chrishenrick@gmail.com>
2018-01-01 18:59:43 -0800

    use tempo display, implement play/pause button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,39 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+
 import { init } from '../utils/metronome';
+
+import TempoDisplay from './TempoDisplay';
 
 class App extends Component {
   static propTypes = {
+    tempo: PropTypes.number.isRequired,
     togglePlayPause: PropTypes.func.isRequired,
+    isPlaying: PropTypes.bool.isRequired,
   };
+
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+  }
 
   componentDidMount() {
     init();
+  }
 
-    // manually invoke to make sure it's working
+  handleClick() {
     this.props.togglePlayPause();
-    setTimeout(() => {
-      this.props.togglePlayPause();
-    }, 5000);
   }
 
   render() {
-    return <div className="App" />;
+    const { isPlaying, tempo } = this.props;
+    const label = isPlaying ? 'pause' : 'play';
+    return (
+      <div className="App">
+        <TempoDisplay tempo={tempo} />
+        <button onClick={this.handleClick}>{label}</button>
+      </div>
+    );
   }
 }
 
