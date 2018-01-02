63ec9105406a93a73e497fa72e8875bd42010257
Chris Henrick <chrishenrick@gmail.com>
2018-01-01 19:57:34 -0800

    🎉  added slider ui component for adjusting tempo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,10 +4,12 @@ import PropTypes from 'prop-types';
 import { init } from '../utils/metronome';
 
 import TempoDisplay from './TempoDisplay';
+import TempoSlider from './TempoSlider';
 import PlayPauseBtn from './PlayPauseBtn';
 
 class App extends Component {
   static propTypes = {
+    setTempo: PropTypes.func.isRequired,
     tempo: PropTypes.number.isRequired,
     togglePlayPause: PropTypes.func.isRequired,
     isPlaying: PropTypes.bool.isRequired,
@@ -18,12 +20,13 @@ class App extends Component {
   }
 
   render() {
-    const { isPlaying, tempo } = this.props;
+    const { isPlaying, setTempo, tempo, togglePlayPause } = this.props;
     const label = isPlaying ? 'pause' : 'play';
     return (
       <div className="App">
         <TempoDisplay tempo={tempo} />
-        <PlayPauseBtn label={label} handleClick={this.props.togglePlayPause} />
+        <TempoSlider handleChange={setTempo} tempo={tempo} />
+        <PlayPauseBtn label={label} handleClick={togglePlayPause} />
       </div>
     );
   }
---
+++ src/components/TempoSlider.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Slider from 'react-rangeslider';
+
+class TempoSlider extends Component {
+  static propTypes = {
+    handleChangeStart: PropTypes.func,
+    handleChange: PropTypes.func,
+    handleChangeComplete: PropTypes.func,
+    tempo: PropTypes.number.isRequired,
+  };
+
+  static defaultProps = {
+    handleChangeStart: () => {},
+    handleChange: () => {},
+    handleChangeComplete: () => {},
+  };
+
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(value) {
+    this.props.handleChange(value);
+  }
+
+  render() {
+    const { handleChangeStart, handleChangeComplete, handleChange, tempo } = this.props;
+    return (
+      <div className="TempoSlider">
+        <Slider
+          min={0}
+          max={250}
+          orientation="horizontal"
+          onChangeStart={handleChangeStart}
+          onChange={value => handleChange(value)}
+          onChangeComplete={handleChangeComplete}
+          value={tempo}
+        />
+      </div>
+    );
+  }
+}
+
+export default TempoSlider;
