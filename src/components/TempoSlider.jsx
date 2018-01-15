a9b37b52545f64c702585634427f7a498605e324
Chris Henrick <chrishenrick@gmail.com>
2018-01-15 15:30:15 -0800

    tempo increment & decrement buttons (#5)
    tempo increment & decrement buttons

--- src/components/TempoSlider.jsx
+++ src/components/TempoSlider.jsx
@@ -4,42 +4,43 @@ import Slider from 'react-rangeslider';
 
 class TempoSlider extends Component {
   static propTypes = {
-    handleChangeStart: PropTypes.func,
-    handleChange: PropTypes.func,
-    handleChangeComplete: PropTypes.func,
+    handleChange: PropTypes.func.isRequired,
     tempo: PropTypes.number.isRequired,
   };
 
-  static defaultProps = {
-    handleChangeStart: () => {},
-    handleChange: () => {},
-    handleChangeComplete: () => {},
-  };
+  handleClick(value) {
+    const { handleChange, tempo } = this.props;
 
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(value) {
-    this.props.handleChange(value);
+    switch (value) {
+      case 'increment':
+        handleChange(tempo + 1);
+        break;
+      case 'decrement':
+        handleChange(tempo - 1);
+        break;
+      default:
+        break;
+    }
   }
 
   render() {
-    const { handleChangeStart, handleChangeComplete, handleChange, tempo } = this.props;
+    const { handleChange, tempo } = this.props;
     return (
       <div className="TempoSlider">
-        <h3>Tempo</h3>
-        <Slider
-          min={0}
-          max={250}
-          orientation="horizontal"
-          onChangeStart={handleChangeStart}
-          onChange={value => handleChange(value)}
-          onChangeComplete={handleChangeComplete}
-          tooltip={false}
-          value={tempo}
-        />
+        <h3 className="title">Tempo</h3>
+        <div className="tempo-slider__controls">
+          <button onClick={() => this.handleClick('decrement')}>–</button>
+          <Slider
+            className="rangeslider__tempo"
+            min={0}
+            max={250}
+            orientation="horizontal"
+            onChange={value => handleChange(value)}
+            tooltip={false}
+            value={tempo}
+          />
+          <button onClick={() => this.handleClick('increment')}>+</button>
+        </div>
       </div>
     );
   }
