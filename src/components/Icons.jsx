c655ea0a1704e3bc71bb9fddcff01f9cca4b7666
Chris Henrick <chrishenrick@gmail.com>
2018-01-15 17:11:33 -0800

    removed xmlns property from svg icons

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -3,13 +3,7 @@ import PropTypes from 'prop-types';
 
 function IconWrapper({ fillColor, width, height }, children) {
   return (
-    <svg
-      fill={fillColor}
-      height={height}
-      viewBox="0 0 24 24"
-      width={width}
-      xmlns="http://www.w3.org/2000/svg"
-    >
+    <svg fill={fillColor} height={height} viewBox="0 0 24 24" width={width}>
       {children}
     </svg>
   );
