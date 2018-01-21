568eb1e22433e5322304a2e6e2404ea14a81c6b0
Chris Henrick <chrishenrick@gmail.com>
2018-01-20 20:59:49 -0800

    updated reducer tests for keep within ranges

--- tests/reducers.spec.js
+++ tests/reducers.spec.js
@@ -25,6 +25,18 @@ describe('reducer', () => {
       ...defaultState,
       isPlaying: true,
     });
+
+    expect(
+      reducer(
+        { ...defaultState, isPlaying: true },
+        {
+          type: types.TOGGLE_PLAY_PAUSE,
+        }
+      )
+    ).toEqual({
+      ...defaultState,
+      isPlaying: false,
+    });
   });
 
   it('should handle SET_TEMPO', () => {
@@ -36,6 +48,26 @@ describe('reducer', () => {
     ).toEqual({
       ...defaultState,
       tempo: 150,
+    });
+
+    expect(
+      reducer(defaultState, {
+        type: types.SET_TEMPO,
+        tempo: 300,
+      })
+    ).toEqual({
+      ...defaultState,
+      tempo: 250,
+    });
+
+    expect(
+      reducer(defaultState, {
+        type: types.SET_TEMPO,
+        tempo: -1,
+      })
+    ).toEqual({
+      ...defaultState,
+      tempo: 1,
     });
   });
 
@@ -49,6 +81,26 @@ describe('reducer', () => {
       ...defaultState,
       meter: 5,
     });
+
+    expect(
+      reducer(defaultState, {
+        type: types.SET_METER,
+        meter: 14,
+      })
+    ).toEqual({
+      ...defaultState,
+      meter: 13,
+    });
+
+    expect(
+      reducer(defaultState, {
+        type: types.SET_METER,
+        meter: 0,
+      })
+    ).toEqual({
+      ...defaultState,
+      meter: 1,
+    });
   });
 
   it('should handle SET_MASTER_VOLUME', () => {
@@ -61,6 +113,26 @@ describe('reducer', () => {
       ...defaultState,
       masterVolume: 0.8,
     });
+
+    expect(
+      reducer(defaultState, {
+        type: types.SET_MASTER_VOLUME,
+        masterVolume: 1.1,
+      })
+    ).toEqual({
+      ...defaultState,
+      masterVolume: 1,
+    });
+
+    expect(
+      reducer(defaultState, {
+        type: types.SET_MASTER_VOLUME,
+        masterVolume: -0.1,
+      })
+    ).toEqual({
+      ...defaultState,
+      masterVolume: 0,
+    });
   });
 
   it('should handle SET_ACCENT_VOLUME', () => {
@@ -72,6 +144,26 @@ describe('reducer', () => {
     ).toEqual({
       ...defaultState,
       accentVolume: 0.5,
+    });
+
+    expect(
+      reducer(defaultState, {
+        type: types.SET_ACCENT_VOLUME,
+        accentVolume: 1.1,
+      })
+    ).toEqual({
+      ...defaultState,
+      accentVolume: 1,
+    });
+
+    expect(
+      reducer(defaultState, {
+        type: types.SET_ACCENT_VOLUME,
+        accentVolume: -1,
+      })
+    ).toEqual({
+      ...defaultState,
+      accentVolume: 0,
     });
   });
 
@@ -97,6 +189,26 @@ describe('reducer', () => {
       ...defaultState,
       eighthVolume: 0.4,
     });
+
+    expect(
+      reducer(defaultState, {
+        type: types.SET_EIGTH_VOLUME,
+        eighthVolume: 1.2,
+      })
+    ).toEqual({
+      ...defaultState,
+      eighthVolume: 1,
+    });
+
+    expect(
+      reducer(defaultState, {
+        type: types.SET_EIGTH_VOLUME,
+        eighthVolume: -0.4,
+      })
+    ).toEqual({
+      ...defaultState,
+      eighthVolume: 0,
+    });
   });
 
   it('should handle SET_SIXTEENTH_VOLUME', () => {
@@ -109,6 +221,26 @@ describe('reducer', () => {
       ...defaultState,
       sixteenthVolume: 0.6,
     });
+
+    expect(
+      reducer(defaultState, {
+        type: types.SET_SIXTEENTH_VOLUME,
+        sixteenthVolume: 1.1,
+      })
+    ).toEqual({
+      ...defaultState,
+      sixteenthVolume: 1,
+    });
+
+    expect(
+      reducer(defaultState, {
+        type: types.SET_SIXTEENTH_VOLUME,
+        sixteenthVolume: -1.1,
+      })
+    ).toEqual({
+      ...defaultState,
+      sixteenthVolume: 0,
+    });
   });
 
   it('should handle SET_TRIPLET_VOLUME', () => {
@@ -120,6 +252,26 @@ describe('reducer', () => {
     ).toEqual({
       ...defaultState,
       tripletVolume: 0.3,
+    });
+
+    expect(
+      reducer(defaultState, {
+        type: types.SET_TRIPLET_VOLUME,
+        tripletVolume: 1.3,
+      })
+    ).toEqual({
+      ...defaultState,
+      tripletVolume: 1,
+    });
+
+    expect(
+      reducer(defaultState, {
+        type: types.SET_TRIPLET_VOLUME,
+        tripletVolume: -0.3,
+      })
+    ).toEqual({
+      ...defaultState,
+      tripletVolume: 0,
     });
   });
 });
