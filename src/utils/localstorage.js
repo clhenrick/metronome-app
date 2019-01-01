fb22eba5e3867f6b329a69294f4343c11675b08d
Chris Henrick <chrishenrick@gmail.com>
2018-12-31 16:01:28 -0800

    added fns for getting & setting app state to/from localstorage

---
+++ src/utils/localstorage.js
@@ -0,0 +1,36 @@
+import {defaultState} from "../reducers";
+
+const storageKey = "metronome-app-state";
+
+export function getSavedState() {
+  const storage = window.localStorage;
+
+  if (!storage) {
+    return defaultState;
+  }
+
+  let state = storage.getItem(storageKey)
+  
+  if (state) {
+    state = JSON.parse(state);
+    state = {
+      ...defaultState,
+      ...state
+    };
+  }
+  
+  return state || defaultState;
+}
+
+export function setSavedState(state) {
+  const storage = window.localStorage;
+  
+  if (!storage) {
+    return;
+  }
+
+  // ignore the isPlaying boolean when saving app state
+  const {isPlaying, ...rest} = state;
+
+  storage.setItem(storageKey, JSON.stringify(rest));
+}
