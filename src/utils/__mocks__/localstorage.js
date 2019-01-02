2523c09aad9c8105631cda50e3c9bd74cac0a6ac
Chris Henrick <chrishenrick@gmail.com>
2019-01-01 20:09:01 -0800

    moved local storage mock to separate module

--- src/utils/__mocks__/localstorage.js
+++ src/utils/__mocks__/localstorage.js
@@ -1,27 +1,3 @@
-class LocalStorageMock {
-  constructor() {
-    this.store = {};
-  }
-
-  clear() {
-    this.store = {};
-  }
-
-  getItem(key) {
-    return this.store[key] || null;
-  }
-
-  setItem(key, value) {
-    this.store[key] = value;
-  }
-
-  removeItem(key) {
-    delete this.store[key];
-  }
-}
-
-global.localStorage = new LocalStorageMock();
-
 const getSavedState = jest.fn();
 const setSavedState = jest.fn();
 
