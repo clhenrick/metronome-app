20aeb3e772bf032ec6013140565c85d089b2e22f
Chris Henrick <chrishenrick@gmail.com>
2019-01-06 18:16:27 -0800

    fixed existing localstorage module tests & added new tests

--- tests/localstorage.spec.js
+++ tests/localstorage.spec.js
@@ -1,14 +1,37 @@
 import { getSavedState, setSavedState } from '../src/utils/localstorage';
-import LocalStorageMock from '../src/utils/__mocks__/browserMocks';
-jest.mock('../src/utils/localstorage.js');
+import { defaultState } from '../src/reducers';
 
-describe('localstorage', () => {
-  const localStorage = new LocalStorageMock();
-
-  it('should access saved state from window.localStorage', () => {
+describe('localstorage module', () => {
+  it('should access any saved state from window.localStorage.getItem', () => {
+    const spy = jest.spyOn(localStorage, 'getItem');
     getSavedState();
-    expect(localStorage.getItem).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledTimes(1);
   });
 
-  it('should save app state using window.localStorage', () => {});
+  it('should return default app state when no state has been previously saved', () => {
+    const state = getSavedState();
+    expect(state).toEqual(defaultState);
+  });
+
+  it('should return a non-default state when state has been previously saved', () => {
+    setSavedState({ tempo: 60 });
+    const state = getSavedState();
+    expect(state).toEqual({
+      ...defaultState,
+      ...state
+    });
+  });
+
+  it('should save app state using window.localStorage.setItem', () => {
+    const spy = jest.spyOn(localStorage, 'setItem');
+    setSavedState({ ...defaultState, tempo: 190 });
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should ignore `isPlaying` when saving state', () => {
+    setSavedState({ isPlaying: true });
+    const state = getSavedState();
+    const expectedState = { ...defaultState };
+    expect(state).toEqual(expectedState);
+  });
 });
