5573aa756653f811eba9965d1ee1e6a9bdd9635b
Chris Henrick <chrishenrick@gmail.com>
2026-05-01 12:06:57 -0700

    chore: use reset css & rm normalize & skeleton

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,8 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import 'normalize.css';
 import './scss/main.scss';
+
 import { MetronomeProvider } from './context/MetronomeContext';
 import App from './components/App';
 
