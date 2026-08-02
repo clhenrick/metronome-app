20f60d3eef706cb4acafe31852bffbc772d66aa5
Chris Henrick <chrishenrick@gmail.com>
2026-08-02 16:31:59 -0700

    feat: added dark/light theme toggle

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,38 +1,43 @@
 import { useMetronome } from '../context/MetronomeContext';
-
 import MeterControl from './MeterControl';
 import MeterDisplay from './MeterDisplay';
 import TempoDisplay from './TempoDisplay';
 import TempoSlider from './TempoSlider';
 import PlayPauseBtn from './PlayPauseBtn';
 import VolumeControls from './VolumeControls';
+import { ThemeToggle } from './ThemeToggle';
 
 function App() {
   const { isPlaying, meter, setMeter, setTempo, tempo, togglePlayPause } = useMetronome();
 
   return (
-    <main className="App">
-      <h1>Metronome App</h1>
-      <section>
-        <h2 id="tempo-label">Tempo</h2>
-        <div className="top-controls-panel">
-          <TempoDisplay tempo={tempo} />
-          <PlayPauseBtn isPlaying={isPlaying} handleClick={togglePlayPause} />
-        </div>
-        <TempoSlider handleChange={setTempo} tempo={tempo} />
-      </section>
-      <section className="meter-panel">
-        <h2>Meter</h2>
-        <div>
-          <MeterDisplay meter={meter} />
-          <MeterControl handleChange={setMeter} meter={meter} />
-        </div>
-      </section>
-      <section>
-        <h2>Volume</h2>
-        <VolumeControls />
-      </section>
-    </main>
+    <>
+      <header>
+        <h1>Metronome App</h1>
+        <ThemeToggle />
+      </header>
+      <main className="App">
+        <section>
+          <h2 id="tempo-label">Tempo</h2>
+          <div className="top-controls-panel">
+            <TempoDisplay tempo={tempo} />
+            <PlayPauseBtn isPlaying={isPlaying} handleClick={togglePlayPause} />
+          </div>
+          <TempoSlider handleChange={setTempo} tempo={tempo} />
+        </section>
+        <section className="meter-panel">
+          <h2>Meter</h2>
+          <div>
+            <MeterDisplay meter={meter} />
+            <MeterControl handleChange={setMeter} meter={meter} />
+          </div>
+        </section>
+        <section>
+          <h2>Volume</h2>
+          <VolumeControls />
+        </section>
+      </main>
+    </>
   );
 }
 
---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+
+export type Theme = 'dark' | 'light';
+
+function getDefaultTheme(): Theme {
+  return (
+    (localStorage.getItem('theme') as Theme | undefined) ||
+    ((window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light') as Theme)
+  );
+}
+
+/** component handles toggling dark / light UI theme */
+export const ThemeToggle = () => {
+  const [theme, setTheme] = useState<Theme>(() => getDefaultTheme());
+  const isDarkTheme = theme === 'dark';
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+
+  function toggleTheme() {
+    setTheme(isDarkTheme ? 'light' : 'dark');
+  }
+
+  const icon = isDarkTheme ? '🌞' : '🌙';
+  const visuallyHiddenText = "Toggle dark theme";
+
+  return (
+    <button className="theme-toggle" onClick={toggleTheme} aria-pressed={isDarkTheme}>
+      <span aria-hidden="true">{icon}</span>
+      <span className="visually-hidden">{visuallyHiddenText}</span>
+    </button>
+  );
+};
